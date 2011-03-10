83bdf308e3361e0962fe4fffae3311bdf42e6c77
Matthew Fay <mattfay@gmail.com>
2011-03-10 12:31:39 -0500

    Basic Bat Code

--- batnball_xna/batnball_xna/Bat.cs
+++ batnball_xna/batnball_xna/Bat.cs
@@ -10,20 +10,57 @@ namespace batnball_xna
 {
     public class Bat
     {
+        public float x;
+        public float y;
 
-        public Bat()
+        public float speed;
+
+        public int player;
+
+        //Bat Size is 150px by 30px
+        Texture2D batTexture;
+
+        public Bat(float startX, float startY, float startSpeed, int startPlayer, Texture2D startTexture)
         {
-
+            x = startX;
+            y = startY;
+            speed = startSpeed;
+            player = startPlayer;
+            batTexture = startTexture;
         }
 
         public void Update(GameTime gameTime)
         {
+            if (player == 1)
+            {
+                if (Keyboard.GetState().IsKeyDown(Keys.W))
+                {
+                    y -=  speed * (float)gameTime.ElapsedGameTime.Milliseconds / 1000f;
+                }
+                if (Keyboard.GetState().IsKeyDown(Keys.S))
+                {
+                    y += speed * (float)gameTime.ElapsedGameTime.Milliseconds / 1000f;
+                }
+            }
 
+            if (player == 2)
+            {
+                if (Keyboard.GetState().IsKeyDown(Keys.Up))
+                {
+                    y -= speed * (float)gameTime.ElapsedGameTime.Milliseconds / 1000f;
+                }
+                if (Keyboard.GetState().IsKeyDown(Keys.Down))
+                {
+                    y += speed * (float)gameTime.ElapsedGameTime.Milliseconds / 1000f;
+                }
+            }
         }
 
         public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
-
+            spriteBatch.Begin();
+            spriteBatch.Draw(batTexture, new Vector2(x, y), Color.White);
+            spriteBatch.End();
         }
     }
 }
